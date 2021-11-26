46ebc99b1b75e91e20664c36c90c83f457cb5e9c
wendellunny <wendelferreiraportugal@gmail.com>
2021-11-26 05:04:51 -0300

    Modificando Migrations e Models de usuários e implementando a autenticação padrão do Laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->integer('is_premium')->default('0')->comment('0 - Não é Conta Premiu, 1 - É conta Premium');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
