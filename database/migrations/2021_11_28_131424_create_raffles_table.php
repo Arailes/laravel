2cb0b7cef1f62a6bae429f3e510fc3ecf04e35b1
wendellunny <wendelferreiraportugal@gmail.com>
2021-11-28 11:19:45 -0300

    raffles table

---
+++ database/migrations/2021_11_28_131424_create_raffles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRafflesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('raffles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->double('price');
+            $table->integer('ticket_amount');
+            $table->integer('sweepstakes_amount');
+            $table->dateTime('sweepstakes_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('raffles');
+    }
+}
