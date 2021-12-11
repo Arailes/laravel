66b9ce7c899d3faffb82b4bff1f3b52b804be937
wendellunny <wendelferreiraportugal@gmail.com>
2021-12-11 17:06:17 -0300

    Estrutura da trait de arquivos

---
+++ app/Traits/FileManager.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App;
+
+trait FileManager{
+
+    protected function createFile($file, String $hash, String $path){
+
+    }
+
+    protected function updateFile($file, String $hash){
+        //
+    }
+
+    protected function deleteFile(Int $id){
+        //
+    }
+}
