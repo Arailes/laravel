77262351cac00022fab1ae2a1d0422774c10a7c9
wendellunny <wendelferreiraportugal@gmail.com>
2022-01-05 22:50:35 -0300

    Image Preview Completo

--- resources/views/app/raffles/raffle-create-page-2.blade.php
+++ resources/views/app/raffles/raffle-create-page-2.blade.php
@@ -11,29 +11,44 @@
                        
                         <label for="file_1">
                             +
-                            <img  alt="" id="image">
+                            <img  alt="" id="image_1" >
                         </label>
-                        <input type="file" id="file_1" image name="file_1">
+                        <input type="file" id="file_1" image name="file_1"   onchange="previewImage(event)">
                     </div>
                     <div class="form-group">
-                        <label for="file_2" >+</label>
-                        <input type="file" id="file_2" image name="file_2">
+                        <label for="file_2" >
+                            +
+                            <img  alt="" id="image_2">
+                        </label>
+                        <input type="file" id="file_2" image name="file_2"  onchange="previewImage(event)">
                     </div>
                     <div class="form-group">
-                        <label for="file_3" >+</label>
-                        <input type="file" id="file_3" image name="file_3">
+                        <label for="file_3" >
+                            +
+                            <img  alt="" id="image_3">
+                        </label>
+                        <input type="file" id="file_3" image name="file_3"  onchange="previewImage(event)">
                     </div>
                     <div class="form-group">
-                        <label for="file_4" >+</label>
-                        <input type="file" id="file_4" image name="file_4">
+                        <label for="file_4" >
+                            +
+                            <img  alt="" id="image_4">
+                        </label>
+                        <input type="file" id="file_4" image name="file_4"  onchange="previewImage(event)">
                     </div>
                     <div class="form-group" >
-                        <label for="file_5" >+</label>
-                        <input type="file" id="file_5" image name="file_5">
+                        <label for="file_5" >
+                            +
+                            <img  alt="" id="image_5">
+                        </label>
+                        <input type="file" id="file_5" image name="file_5"  onchange="previewImage(event)">
                     </div>
                     <div class="form-group">
-                        <label for="file_6" >+</label>
-                        <input type="file"id="file_6" image name="file_6">
+                        <label for="file_6" >
+                            +
+                            <img  alt="" id="image_6">
+                        </label>
+                        <input type="file"id="file_6" image name="file_6"  onchange="previewImage(event)">
                     </div>
                 </div>
                 <div class="form-group">
@@ -48,12 +63,18 @@
 
 @push('js')
     <script>
-        const input = document.querySelector('#file_1');
-        const image = document.querySelector('#image');
-
-        input.addEventListener('change',function(event){
-            image.src = URL.createObjectURL(event.target.files[0])
+        function previewImage (event){
+            console.log('entrou');
+            const id = event.target.getAttribute('id');
+            const index = id.substring(5,6)
             
-        })
+            const image = document.querySelector(`#image_${index}`);
+
+           
+            image.src = URL.createObjectURL(event.target.files[0])
+                
+           
+        }
+        
     </script>
 @endpush
