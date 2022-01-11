18c0e1752071cbb13a229a5acc210b9ce763636e
wendellunny <wendelferreiraportugal@gmail.com>
2022-01-10 23:15:04 -0300

    Iniciando estilização da listagem das rifas

--- resources/views/app/raffles/my-raffles.blade.php
+++ resources/views/app/raffles/my-raffles.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.app.layout')
 
 @section('content')
-    <h3>Minhas Rifas</h3>
-    <ul>
-        @foreach ($raffles as $raffle)
-            <li>
-                <div>
-                    <span>{{$raffle->title}}</span>  
-                </div>
-                <div>
-                    @foreach ($raffle->images as $image)
-                        <img src="{{Storage::url($image->path)}}" alt="{{$image->title}}">   
-                    @endforeach
-                </div>
-                <div>
-                    <a href="">Mais Informações</a>
-                </div>
-            </li>
-        @endforeach
-    </ul>
+    <div class="my-raffles-title">
+        <h3>Minhas Rifas</h3>
+    <div>
+    <div class="my-raffles-body">
+        <ul>
+            @foreach ($raffles as $raffle)
+                <li>
+                    <div>
+                        <span>{{$raffle->title}}</span>  
+                    </div>
+                    <div>
+                        @foreach ($raffle->images as $image)
+                            <img src="{{Storage::url($image->path)}}" alt="{{$image->title}}">   
+                        @endforeach
+                    </div>
+                    <div>
+                        <a href="">Mais Informações</a>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    
 @endsection
 
